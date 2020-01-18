439e7392601f30bda80a96b922ee75450311f4ea
Mohit Bansal <bansalmohit9414@gmail.com>
2020-01-18 19:47:04 +0530

    Commit to Pig Game

--- app.js
+++ app.js
@@ -1,19 +1,7 @@
-/*
-GAME RULES:
-
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
-*/
-
-
-var scores, roundScore, activePlayer;
+var scores, roundScore, activePlayer, isfinish;
 
 //document.querySelector('#current-'+activePlayer).textContent = dice;
-//document.querySelector('#current-'+activePlayer).innerHTML = '<em>'+dice+'</em>'ry
+//document.querySelector('#current-'+activePlayer).innerHTML = '<em>'+dice+'</em>';
 //var x = document.querySelector('#current-'+activePlayer).textContent;
 //console.log(x);
 
@@ -23,39 +11,46 @@ initialize();
 
 document.querySelector('.btn-roll').addEventListener('click',function(){
     
-    var dice = Math.floor(6*Math.random()+1);
-    var dom = document.querySelector('.dice');
+   if(!isfinish)
+       {
+            var dice = Math.floor(6*Math.random()+1);
+            var dom = document.querySelector('.dice');
     
-    if(dice===1)
-        {
-            nextPlayer();
-        }
-         else
-        {
-            dom.style.display = 'block';
-             dom.src = 'dice-'+dice+'.png'; 
-             roundScore += dice;
-             document.getElementById('current-'+activePlayer).textContent = roundScore;
-        }
+            if(dice===1)
+            {
+                nextPlayer();
+            }
+             else
+             {
+                 dom.style.display = 'block';
+                 dom.src = 'dice-'+dice+'.png'; 
+                 roundScore += dice;
+                 document.getElementById('current-'+activePlayer).textContent = roundScore;
+             }
+       }
 });
 
 
 
 document.querySelector('.btn-hold').addEventListener('click',function(){
     
-    scores[activePlayer] += roundScore;
-    document.getElementById('score-'+activePlayer).textContent = scores[activePlayer];
+    if(!isfinish)
+        {
+            scores[activePlayer] += roundScore;
+            document.getElementById('score-'+activePlayer).textContent = scores[activePlayer];
     
-    if(scores[activePlayer] >=100)
-        {
-            document.querySelector('#name-'+activePlayer).textContent = 'Winner';
-            document.querySelector('.dice').style.display = 'none';
-            document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
-            document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
-        }
-    else
-        {
-            nextPlayer();
+            if(scores[activePlayer] >=100)
+                {
+                        document.querySelector('#name-'+activePlayer).textContent = 'Winner';
+                        document.querySelector('.dice').style.display = 'none';
+                        document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
+                        document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
+                        isfinish = true;
+                }
+            else
+                {
+                        nextPlayer();
+                }
         }
     
 });
@@ -76,6 +71,7 @@ function initialize()
     scores = [0,0];
     roundScore = 0;
     activePlayer = 0;
+    isfinish = false;
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
     document.getElementById('current-0').textContent = '0';
