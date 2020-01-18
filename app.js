faf82ffde054b1c8bd8f90fe4911fff0a8072fef
Mohit Bansal <bansalmohit9414@gmail.com>
2020-01-18 18:50:39 +0530

    Commit to Pig Game

--- app.js
+++ app.js
@@ -49,6 +49,9 @@ document.querySelector('.btn-hold').addEventListener('click',function(){
     if(scores[activePlayer] >=100)
         {
             document.querySelector('#name-'+activePlayer).textContent = 'Winner';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
+            document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
         }
     else
         {
